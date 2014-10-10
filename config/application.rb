16a26a76787be177ec20a7db1403a024dd70d41b
gearsgrinding <hualex@seas.upenn.edu>
2014-10-09 23:43:50 -0400

    picking up again

--- config/application.rb
+++ config/application.rb
@@ -7,7 +7,10 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module Pinteresting
+
   class Application < Rails::Application
+
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
